e0e82e48e59a6275cb6d57e74bdca16aca7fe6ed
Bruce Jernell <59374529+opconWizard@users.noreply.github.com>
2020-02-18 08:44:57 -0500

    Added additional functionality and error checks

--- Winscp.ps1
+++ Winscp.ps1
@@ -1,50 +1,96 @@
 ﻿param (
+    [string] $winscpPath = "C:\Program Files (x86)\WinSCP\WinSCPnet.dll",
     [string] $localPath,
     [string] $remotePath,
     [string] $hostname,
     [string] $user,
     [string] $password,
-    [string] $fileName
+    [string] $fileName,
+    [string] $option
 )
          
 try
 {
     # Load WinSCP .NET assembly
-    Add-Type -Path "C:\Program Files (x86)\WinSCP\WinSCPnet.dll"
+    Add-Type -Path "$winscpPath"
+}
+catch [Exception]
+{
+    Write-Host "Error: "$_.Exception.Message
+    Exit 1
+}
 
-    # Setup session options
-    $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
-        Protocol = [WinSCP.Protocol]::ftp
-        HostName = $hostname
-        UserName = $user
-        Password = $password
-        #SshHostKeyFingerprint = "ssh-rsa 2048 xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx"
-    }
+# Setup session options
+$sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+    Protocol = [WinSCP.Protocol]::ftp
+    HostName = $hostname
+    UserName = $user
+    Password = $password
+    #SshHostKeyFingerprint = "ssh-rsa 2048 xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx"
+}
  
-    $session = New-Object WinSCP.Session
+$session = New-Object WinSCP.Session
  
+if($option -eq "upload")
+{
     try
     {
         # Connect
         $session.Open($sessionOptions)
- 
-        # Format timestamp
-        #$stamp = $(Get-Date -Format "yyyyMMddHHmmss")
- 
-        # Download the file and throw on any error
-        $session.GetFiles(($remotePath + $fileName),($localPath + $fileName)).Check()
-        #Write-Host $session.Output 
+
+        # Upload files
+        $transferOptions = New-Object WinSCP.TransferOptions
+        #$transferOptions.TransferMode = [WinSCP.TransferMode]::Binary
+        $transferResult = $session.PutFiles($localPath,($remotePath + $filename), $False, $transferOptions)
+    
+        # Throw on any error
+        $transferResult.Check()
+        Write-Host $session.Output
+    
+        # Print results
+        foreach ($transfer in $transferResult.Transfers)
+        {
+            Write-Host "Upload of $($transfer.FileName) succeeded"
+        }
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 7
     }
     finally
     {
         # Disconnect, clean up
         $session.Dispose()
     }
- 
-    exit 0
 }
-catch [Exception]
+elseif($option -eq "download")
 {
-    Write-Host ("Error: {0}" -f $_.Exception.Message)
-    exit 1
+    try
+    {
+        # Connect
+        $session.Open($sessionOptions)
+ 
+        # Download the file and throw on any error
+        $sessionResult = $session.GetFiles($remotePath + $fileName),($localPath + $fileName)
+        
+        # Throw error if found
+        $sessionResult.Check()
+        Write-Host $session.Output
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 7
+    }
+    finally
+    {
+        # Disconnect, clean up
+        $session.Dispose()
+    }    
+}
+else 
+{
+    Write-Host "Option not specified, must be 'upload' or 'download'"
+    Exit 8
 }
